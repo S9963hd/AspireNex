2031836ceb33cb8f3497c9c0341435c738f5ef57
S9963hd <76931140+S9963hd@users.noreply.github.com>
2024-07-06 16:03:18 +0530

    Update Login.jsx

--- src/LoginAndSignUp/Login.jsx
+++ src/LoginAndSignUp/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
     const {login,setLogin}=useContext(LoginDetails);
     const[loading,setLoading]=useState(false);
     const[email,setEmail]=useState('');
+    const[floading,setFloading]=useState(false);
      function retrivepassword(email){
         axios({
             url:"http://localhost:8010/forgot",
@@ -29,6 +30,7 @@ const Login = () => {
         else if(status==201){
             toast.success("Check you're Gmail");
             setLoading(false);
+            setFloading(false);
         }
         else if(status==401){
             toast.error("Gmail Not found");
@@ -58,7 +60,7 @@ const Login = () => {
             <h3 style={{color:'#282828'}}>Enter your Password</h3>
             <input type={(showPassword)?"password":'text'} id="password" />
             <p className="showPassword" onClick={()=>setShowPassword(!showPassword)}>showpassword</p>
-            <p className="showPassword" onClick={()=>retrivepassword(email)}>forgotpassword?</p>
+            <p className="showPassword" onClick={()=>{retrivepassword(email);setFloading(!floading}}>forgotpassword?&nbsp;{(floading)?<p class="fa-solid fa-spinner loading"></p>:""}</p>
             <div className="content1">
                 <button type="button" className="button" style={{marginRight:'10px'}} onClick={()=>{loggingIn();setLoading(!loading)}}>Login&nbsp;{(loading)?<i class="fa-solid fa-spinner loading"></i>:""}</button>
                 <button type="button" className="button" onClick={()=>navigate('/signup')}>SignUp</button>
